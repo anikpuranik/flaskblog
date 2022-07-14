e8a2dd775dfd79fa163b6bb2b487ed0d82467140
anikpuranik <aniketpuranik1995@gmail.com>
2022-07-14 20:24:45 +0530

    creating templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to World of Billionares</h1>"
+        return render_template("home.html")
